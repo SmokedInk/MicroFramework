45b487952d0cb090e07835ffbc0336bef3914121
SmokedInk <15194654202@163.com>
2019-12-26 15:54:47 +0800

    更新CSV读写模式，新增JSON读写及param.json文件

--- IOFile/IOCsv.py
+++ IOFile/IOCsv.py
@@ -6,16 +6,17 @@
 import csv
 
 
-def write_csv(file_path, column_headline, data_text):
+def write_csv(file_path, column_headline, data_text, write_mode='w'):
     """
     写入CSV文件数据
     :param file_path: 文件路径
     :param column_headline: 列标题
     :param data_text: 待写入数据
+    :param write_mode: 写入模式
     :return: None
     """
     # 创建CSV文件       newline=""此参数不加会出现空行
-    csv_file = open(file_path, "w", encoding="utf-8", newline="")
+    csv_file = open(file_path, write_mode, encoding="utf-8", newline="")
     # 创建写入的对象   dialect=""此参数表示的文件的类型
     writer = csv.writer(csv_file)
     # 写入单行用writerow(可用于写入行标题)
@@ -26,14 +27,15 @@ def write_csv(file_path, column_headline, data_text):
     csv_file.close()
 
 
-def read_csv(file_path):
+def read_csv(file_path, read_mode='r'):
     """
     读取CSV文件数据
     :param file_path: 文件路径
+    :param read_mode: 读取模式
     :return: 文件内容
     """
     # 读取CSV文件
-    csv_file = open(file_path, "r", encoding="utf-8")
+    csv_file = open(file_path, read_mode, encoding="utf-8")
     # 获取文件数据
     reader = csv.reader(csv_file)
     csv_values = []
@@ -43,8 +45,8 @@ def read_csv(file_path):
 
 
 if __name__ == '__main__':
-    file_path = r"../data/csvTest.csv"
+    filePath = r"../data/csvTest.csv"
     data_01 = ["index", "a_name", "b_name"]
     data_02 = [[1, "a", "b"], [2, "c", "d"], [3, "e", "f"]]
-    # write_csv(file_path, data_01, data_02)
-    read_csv(file_path)
+    # write_csv(filePath, data_01, data_02)
+    read_csv(filePath)
